b1ead74f64ef1e82b47aabfd9a6b9a0d91010c01
Sergio Pérez-Montero <98036930+sperezmont@users.noreply.github.com>
2023-03-07 14:01:36 +0100

    Add files via upload

---
+++ src/forcing/orbital.jl
@@ -0,0 +1,110 @@
+# =============================
+#     Program: orbital.jl
+#     Aim: This program contains functions to calculate orbital parameters
+# =============================
+@doc """
+    calc_artificial_insolation:
+        Compute daily average insolation through different parameterizations
+"""
+function calc_artificial_insolation(now_o, par_o)
+    # Calculate reference and amplitude
+    ins_ref = (par_o["ins_max"] + par_o["ins_min"]) / 2
+    A_ins = (par_o["ins_max"] - par_o["ins_min"]) / 2
+
+    # Return artificial insolation -- I have to discuss this with jas
+    ins = ins_ref + A_ins * (
+        par_o["P_obl"] * cos(2.0 * pi * now_o["time"] / par_o["tau_obl"]) +
+        par_o["P_pre"] * cos(2.0 * pi * now_o["time"] / par_o["tau_pre"]) +
+        par_o["P_exc"] * cos(2.0 * pi * now_o["time"] / par_o["tau_exc"]))
+    for hm in hemisphere
+        now_o["ins_"*hm] = ins
+    end
+    return now_o
+end
+
+@doc """
+    calc_solar_longitude:
+        Compute solar longitude given day of the year and orbital parameters
+        Adapted from The Climate Laboratory of Brian E. J. Rose (https://brian-rose.github.io/ClimateLaboratoryBook/home.html)
+"""
+function calc_solar_longitude(day, long_peri, ecc; days_per_year=365.2422)
+    long_peri_rad = deg2rad(long_peri)
+    delta_omega = (day - 80.0) * 2 * pi / days_per_year
+    beta = sqrt(1 - ecc^2)
+
+    omega_long_m = -2 * ((ecc / 2 + (ecc^3) / 8) * (1 + beta) * sin(-long_peri_rad) -
+                         (ecc^2) / 4 * (1 / 2 + beta) * sin(-2 * long_peri_rad) +
+                         (ecc^3) / 8 * (1 / 3 + beta) * sin(-3 * long_peri_rad)) + delta_omega
+
+    omega_long = omega_long_m + (2 * ecc - (ecc^3) / 4) * sin(omega_long_m - long_peri_rad) +
+                 (5 / 4) * (ecc^2) * sin(2 * (omega_long_m - long_peri_rad)) +
+                 (13 / 12) * (ecc^3) * sin(3 * (omega_long_m - long_peri_rad))
+    return omega_long
+end
+
+@doc """
+    calc_laskar_insolation: 
+        Compute daily average insolation given latitude and day of the year
+        Adapted from The Climate Laboratory of Brian E. J. Rose (https://brian-rose.github.io/ClimateLaboratoryBook/home.html)
+"""
+function calc_laskar_insolation(t; lat=65.0, day=170.0, S0=1365.2, day_type=1, days_per_year=365.2422)
+    # First, calculate the orbital parameters at t (years) since J2000 epoch
+    long_peri, obliquity, ecc = orbital_params(t) # -- using Insolation.jl (rad, rad, --)
+    long_peri, obliquity = rad2deg(long_peri), rad2deg(obliquity)
+    phi = deg2rad(lat)
+
+    # -- calculate solar longitude
+    if day_type == 1
+        omega_long = calc_solar_longitude(day, long_peri, ecc, days_per_year=days_per_year)
+    elseif day_type == 2
+        omega_long = deg2rad(day)
+    end
+
+    # -- declination angle of the Sun
+    delta = asin(sin(deg2rad(obliquity)) * sin(omega_long))
+
+    # -- hour angle at sunrise / sunset
+    if abs(delta) - pi / 2 + abs(phi) < 0.0 # there is sunset/sunrise
+        Ho = acos(-tan(phi) * tan(delta))
+    else # otherwise figure out if it's all night or all day
+        if phi * delta > 0.0
+            Ho = pi
+        else
+            Ho = 0.0
+        end
+    end
+
+    # -- cos zenith angle
+    coszen = Ho * sin(phi) * sin(delta) + cos(phi) * cos(delta) * sin(Ho)
+
+    # Compute insolation as in Berger 1978 (equation 10)
+    Fsw = S0 / pi * ((1 + ecc * cos(omega_long - deg2rad(long_peri)))^2 / (1 - ecc^2)^2 * coszen)
+    return Fsw
+end
+
+@doc """
+    calc_insol_day:
+        Compute daily average insolation
+"""
+function calc_ins(now_o, par_o)
+    if par_o["ins_case"] == "constant"
+        for hm in par_o["hemisphere"]
+            now_o["ins_"*hm] = copy(par_o["ins_const"])
+        end
+    elseif par_o["ins_case"] == "artificial"
+        now_o = calc_artificial_insolation(now_o, par_o)
+    elseif par_o["ins_case"] == "laskar"
+        for hm in par_o["hemisphere"]
+            now_o["ins_"*hm] = calc_laskar_insolation(now_o["time"],
+                lat=par_o["ins_lat_"*hm],
+                day=par_o["ins_day"],
+                S0=S_0,
+                day_type=1,
+                days_per_year=year_len)
+        end
+    else
+        error("ERROR, insolation option not recognized")
+    end
+    return now_o
+end
+
---
+++ src/forcing/radiative.jl
@@ -0,0 +1,200 @@
+# =============================
+#     Program: radiative.jl
+#     Aim: This program contains functions to calculate radiative parameters
+# =============================
+@doc """
+    calc_rad_co2:
+        calculates the radiative forcing due to co2 (in W/m²)
+"""
+function calc_rad_co2(CO2::Real)
+    CO2_0 = 280.0
+    RCO2_fac = 5.35
+    return RCO2_fac * log(CO2 / CO2_0)
+end
+
+@doc """
+    calc_albedo_ref:
+        calculates reference value for albedo
+"""
+function calc_albedo_ref(now_r::OrderedDict, par_r::OrderedDict)
+    for hm in par_r["hemisphere"]
+        if now_r["H_"*hm] == 0.0
+            now_r["ice_time_"*hm] = 0.0 # -- no ice
+            now_r["albedo_ref_"*hm] = par_r["albedo_land"]
+        else
+            n = 1 # exponent number in albedo-age parameterisation
+            now_r["albedo_ref_"*hm] = max(par_r["albedo_newice"] - par_r["albedo_slope"] * now_r["ice_time_"*hm]^n, par_r["albedo_oldice"])
+        end
+    end
+    return now_r
+end
+
+@doc """
+    calc_T_rf:
+        calculates radiative forcing for each hemisphere
+"""
+function calc_T_rf(now_r::OrderedDict, par_r::OrderedDict)
+    for hm in par_r["hemisphere"]
+        # -- insolation
+        now_r = calc_ins(now_r, par_r)
+        now_r["ins_anom_"*hm] = now_r["ins_"*hm] - par_r["ins_ref_"*hm]
+        # -- radiative forcing from co2
+        radco2 = calc_rad_co2(now_r["co2_"*hm])
+        # -- total        
+        now_r["T_rf_"*hm] = par_r["csi"] * now_r["ins_anom_"*hm] + par_r["cs"] * radco2
+
+    end
+    return now_r
+end
+
+@doc """
+    calc_T_sl:
+        calculates sea level temperature
+"""
+function calc_T_sl(now_r::OrderedDict, par_r::OrderedDict)
+    # First, calculate insolation
+    now_r = calc_ins(now_r, par_r)
+
+    for hm in par_r["hemisphere"]
+        # -- normalize insolation
+        insnorm = (now_r["ins_"*hm] - par_r["ins_min"]) / (par_r["ins_max"] - par_r["ins_min"])     # between 0 and 1, norm = 1
+        now_r["ins_norm_"*hm] = 2.0 * insnorm - 1.0                                                          # between 1 and -1, norm = 2
+
+        # Second, compute anthropogenic forcing if time >= time_anth
+        if (now_r["time"] >= par_r["time_anth"])
+            T_anth = par_r["At_anth"] / exp((now_r["time"] - par_r["time_anth"]) / par_r["tau_anth"])
+        else
+            T_anth = 0.0
+        end
+
+        # Finally, calculate sea-level temperature 
+        now_r["T_sl_"*hm] = par_r["T_ref0_"*hm] + par_r["A_t"] * now_r["ins_norm_"*hm] + T_anth
+    end
+    return now_r
+end
+
+"""
+    calc_temp_and_tempref(now, par, hm)
+selects temperature to use and computes reference temperature desired
+
+## Attributes
+* `now` Dictionary with values of the model variables at current timestep
+* `par` Dictionary with run parameters
+* `hm` Hemisphere in which we calculate ("_n" or "_s")
+
+## Return
+`temp` and `now["T_ref_"*hm]` to use
+"""
+function calc_temp_and_tempref(now::OrderedDict, par::OrderedDict, hm)
+    if par["active_climate"]
+        if par["height_temp"] == "useH"
+            temp = now["T_surf_"*hm]
+        elseif par["height_temp"] == "useZ"
+            temp = now["T_"*hm]
+        else
+            printstyled("dev par must be removed!", color=:red)
+        end
+
+    else
+        temp = now["T_surf_"*hm]  # if only dynamics, we take into account the lapse rate here
+    end
+
+    # Anthropogenic forcing?
+    if now["time"] < par["time_anth"] # unperturbed climate
+        if par["height_temp"] == "useH"
+            now["T_ref_"*hm] = par["T_ref0_"*hm] - grad * now["Z_"*hm]
+        elseif par["height_temp"] == "useZ"
+            now["T_ref_"*hm] = par["T_ref0_"*hm]
+        end
+    else    # perturbed climate
+        if par["height_temp"] == "useH"
+            now["T_ref_"*hm] = par["T_ref0_"*hm] - grad * now["Z_"*hm]
+            now["T_ref_"*hm] += par["cco2"] * now["co2_"*hm] / par["co2_ref"]
+        elseif par["height_temp"] == "useZ"
+            now["T_ref_"*hm] = par["T_ref0_"*hm] + par["cco2"] * now["co2_"*hm] / par["co2_ref"]
+        end
+    end
+
+    return temp, now["T_ref_"*hm]
+end
+
+#############################
+# Time derivatives
+#############################
+@doc """
+    calc_Tdot:
+        calculates regional temperature derivative
+"""
+function calc_Tdot(now, par)
+    for hm in par["hemisphere"]
+
+        # Anthropogenic forcing?
+        if now["time"] < par["time_anth"] # unperturbed climate
+            now["T_ref_"*hm] = par["T_ref0_"*hm]
+        else    # perturbed climate
+            now["T_ref_"*hm] = par["T_ref0_"*hm] + par["cco2"] * now["co2_"*hm] / par["co2_ref"]
+        end
+
+        if par["height_temp"] == "useH"
+            HTF = now["H_"*hm]
+        elseif par["height_temp"] == "useZ"
+            HTF = now["Z_"*hm]
+        else
+            printstyled("dev par must be removed!", color=:red)
+        end
+
+        now["Tdot_"*hm] = (now["T_ref_"*hm] - now["T_"*hm]
+                              +
+                              now["T_rf_"*hm]
+                              -
+                              par["csz"] * HTF) / par["tau_rf_"*hm]
+    end
+    return now
+end
+
+@doc """
+    calc_albedodot:
+        calculates albedo derivative
+"""
+function calc_albedodot(now, par)
+    for hm in par["hemisphere"]
+        now["albedodot_"*hm] = (now["albedo_ref_"*hm] - now["albedo_"*hm]) / par["tau_albedo"]
+
+        if now["H_"*hm] == 0.0
+            now["albedo_"*hm] = par["albedo_land"]
+            now["albedodot_"*hm] = 0.0
+        elseif now["ice_time_"*hm] < 10.0 # First ice
+            now["albedo_"*hm] = par["albedo_newice"]
+            now["albedodot_"*hm] = 0.0
+        end
+    end
+    return now
+end
+
+@doc """
+    calc_co2dot:
+        calculates co2 derivative
+"""
+function calc_co2dot(now, par)
+    for hm in par["hemisphere"]
+
+        # -- anthropogenic forcing?
+        if now["time"] < par["time_anth"] # unperturbed climate
+            co2ref = par["co2_ref"]
+        else    # perturbed climate
+            actual_diftime = now["time"] - par["time_anth"]
+            co2ref = par["co2_ref"] + par["co2_anth"] * (
+                0.75 / exp(actual_diftime / 365.0)  # ocean invasion
+                + 0.135 / exp(actual_diftime / 5500.0)  # sea floor CaCO3 neutralization 
+                + 0.035 / exp(actual_diftime / 8200.0)  # terrestrial floor CaCO3 neutralization
+                + 0.08 / exp(actual_diftime / 200000.0) # silicate weathering
+            )   # Archer 1997
+        end
+
+        temp, now["T_ref_"*hm] = calc_temp_and_tempref(now, par, hm)
+        now["co2dot_"*hm] = (co2ref - now["co2_"*hm] + par["ktco2"] * (temp - now["T_ref_"*hm])) / par["tau_co2"]
+    end
+    return now
+end
+
+
